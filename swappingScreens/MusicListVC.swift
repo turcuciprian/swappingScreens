65fe4ff4e61fa37e2add6e99b0b451619c9ca880
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-07 13:05:17 +0300

    going to another controll using code (dismiss)

--- swappingScreens/MusicListVC.swift
+++ swappingScreens/MusicListVC.swift
@@ -21,6 +21,9 @@ class MusicListVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
